4aea09694b126e16c2e446cad64134a0e47af85c
f.foguefono@condorcet.be <f.foguefono@condorcet.be>
2024-06-03 15:29:01 +0200

    mvc design pattern

--- course/src/designpatterns/observer/Observer.java
+++ course/src/designpatterns/observer/Observer.java
@@ -1,22 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package designpatterns.observer;
+package mvc.observer;
 
+import java.util.List;
 
-public class Coureur {
-    private String nom;
-
-    public Coureur(String nom) {
-        this.nom = nom;
-    }
-
-    public void update(Course course) {
-        System.out.println(nom + " est notifié de la course : " + course.getNom());
-        // Réagir à la notification de la course
-    }
-
-    // Autres méthodes spécifiques à la classe Coureur
+public interface Observer {
+    void update(Course course);
 }
--- course/src/designpatterns/observer/Subject.java
+++ course/src/designpatterns/observer/Subject.java
@@ -1,31 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package designpatterns.observer;
+package mvc.observer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- *
- * @author Michel
- */
-public abstract class Subject {
-   protected List<Observer> myObservers = new ArrayList<>();
-  
-   public void addObserver(Observer o){
-       myObservers.add(o);
-   }
-   public void removeObserver(Observer o){
-       myObservers.remove(o);
-   }
-   public void notifyObservers(){
-       String msg=getNotification();
-       for(Observer o : myObservers) o.update(msg);
-   }
-   
-   public abstract String getNotification();
-  
+public interface Subject {
+    void addObserver(Observer observer);
+    void removeObserver(Observer observer);
+    void notifyObservers();
 }
+
